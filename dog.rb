0558e19123b299325c1169a54a06ca07bed24d6f
Kellyx1636 <kellyx1636@hstat.org>
2018-01-19 16:34:02 +0000

    Set up the empty methods for each file.

--- dog.rb
+++ dog.rb
@@ -2,5 +2,26 @@ class Dog
     attr_accessor 
     
     def initialize
+        
     end 
+    
+    def wellness 
+        
+    end 
+    
+    def health 
+        
+    end 
+    
+    def happiness
+        
+    end 
+    
+    def eat 
+        
+    end 
+    
+    def exercise 
+        
+    end
 end 
--- owner.rb
+++ owner.rb
@@ -2,5 +2,26 @@ class Owner
     attr_accessor 
     
     def initialize
+        
     end 
+    
+    def wellness 
+        
+    end 
+    
+    def health 
+        
+    end 
+    
+    def happiness
+        
+    end 
+    
+    def eating 
+        
+    end 
+    
+    def exercise 
+        
+    end
 end 
